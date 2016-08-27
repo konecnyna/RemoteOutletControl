2f4fe6fc314e543dbd94817560e6d4a2719e0c86
Nick <konecnyna@gmail.com>
2016-08-27 19:04:44 -0400

    Revamp of remote python script

--- python/ac_outlet_control.py
+++ python/ac_outlet_control.py
@@ -15,143 +15,79 @@
 # F - Parity bit.
 
 import wiringpi2 as wiringpi
-from time import sleep 
+from time import sleep
 import sys
-
-#CONST
-SIGNAL_SEND_ATTEMPTS = 30
-TRANSMIT_PIN = 24
-SHORT_DELAY_ON = 330
-SHORT_DELAY_OFF = 900
-LONG_DELAY_ON = 920
-LONG_DELAY_OFF = 305
-SIGNAL_BEGIN = 2000
-SIGNAL_ARRAY_SIZE = 25
-STATE_OFF = 0
-STATE_ON = 1
-
-#GLOBALS
-allOnSignal = [0,1,1,0,1,1,1,0,1,1,0,0,1,1,1,0,1,0,0,1,1,0,0,1,0]
-allOffSignal = [0,1,1,0,1,1,1,0,1,1,0,0,1,1,1,0,1,0,0,1,0,0,0,1,0]
+import remotes
 
 #FUNCTIONS
+TRANSMIT_PIN = 24
+
+
 def main(args):
 	try:
 		initPins()
 
-		input_outlet = int(args[1])
-		input_state = int(args[2])
-
-		sendSignal(createSignal(input_outlet,input_state))
+		type = args[1]
+		input_outlet = int(args[2])
+		input_state = int(args[3])
+		
+		if type == remotes.ByeBye.NAME:
+			sendSignal(remotes.ByeBye(), input_outlet, input_state)	
+		elif type == remotes.Zap.NAME:
+			sendSignal(remotes.Zap(), input_outlet, input_state)
+		else:
+			print "Bad type. try 'zap' or 'byebye'"
 
 	finally:
 		wiringpi.digitalWrite(TRANSMIT_PIN,0)
-		wiringpi.pinMode(TRANSMIT_PIN, 0)  
+		wiringpi.pinMode(TRANSMIT_PIN, 0)
 
 
 def initPins():
 	wiringpi.wiringPiSetupGpio()
-	# sets GPIO 24 to output 
+	# sets GPIO 24 to output
 	wiringpi.pinMode(TRANSMIT_PIN, 1)
 	# sets port 24 to 0 (0V, off)
 	wiringpi.digitalWrite(TRANSMIT_PIN,0)
 
 
-def sendSignal(signalWaveForm):
+def sendSignal(signalObject, input_outlet, input_state):
 	attempts = 0
-	
-	while(attempts < SIGNAL_SEND_ATTEMPTS):
+
+	while(attempts < signalObject.SIGNAL_SEND_ATTEMPTS):
 		if (attempts % 10 == 0) :
 			sleep(0.1)
-			
-		transmitWaveForm(2)
-		for value in signalWaveForm:
-			transmitWaveForm(value)
 
-		transmitWaveForm(2)
+		transmitWaveForm(2, signalObject)
+		for value in signalObject.createSignal(input_outlet, input_state):
+			transmitWaveForm(value, signalObject)
+
+		transmitWaveForm(2, signalObject)
 		attempts += 1
 
 	print "Signal complete"
 
-def transmitWaveForm(waveForm):
+def transmitWaveForm(waveForm, signalObject):
 	if(waveForm == 0):
 		wiringpi.digitalWrite(TRANSMIT_PIN,1)
-		wiringpi.delayMicroseconds(SHORT_DELAY_ON)
+		wiringpi.delayMicroseconds(signalObject.SHORT_DELAY_ON)
 		wiringpi.digitalWrite(TRANSMIT_PIN,0)
-		wiringpi.delayMicroseconds(SHORT_DELAY_OFF)
+		wiringpi.delayMicroseconds(signalObject.SHORT_DELAY_OFF)
 	elif(waveForm == 1):
 		wiringpi.digitalWrite(TRANSMIT_PIN,1)
-		wiringpi.delayMicroseconds(LONG_DELAY_ON)
+		wiringpi.delayMicroseconds(signalObject.LONG_DELAY_ON)
 		wiringpi.digitalWrite(TRANSMIT_PIN,0)
-		wiringpi.delayMicroseconds(LONG_DELAY_OFF)
+		wiringpi.delayMicroseconds(signalObject.LONG_DELAY_OFF)
 	elif(waveForm == 2):
 		wiringpi.digitalWrite(TRANSMIT_PIN,0)
-		wiringpi.delayMicroseconds(SIGNAL_BEGIN)
+		wiringpi.delayMicroseconds(signalObject.SIGNAL_BEGIN)
 
 
-#SIGNAL CONSTS
-BASE_WAVE_FORM = [0,1,1,0,1,1,1,0,1,1,0,0,1,1,1,0,1,0,0,1]
-#Outlet wave bits
-OUTLET_ALL_ON =  [1,0,0,1,0]
-OUTLET_ALL_OFF = [0,0,0,1,0]
-OUTLET_1_ON =    [1,1,1,1,0]
-OUTLET_1_OFF =   [0,1,1,1,0]
-OUTLET_2_ON =    [1,1,0,1,0]
-OUTLET_2_OFF =   [0,1,0,1,0]
-OUTLET_3_ON =    [1,0,1,1,0]
-OUTLET_3_OFF =   [0,0,1,1,0]
-OUTLET_4_ON =    [1,1,1,0,0]
-OUTLET_4_OFF =   [0,1,1,0,0]
-OUTLET_5_ON =    [1,1,0,0,0]
-OUTLET_5_OFF =   [0,1,0,0,0]
-OUTLET_6_ON =    [1,0,1,0,0]
-OUTLET_6_OFF =   [0,0,1,0,0]
 
-def createSignal(outlet, state):
-	newWaveForm = BASE_WAVE_FORM[:]
-	if(outlet == 0):
-		if(state == STATE_OFF):
-			newWaveForm.extend(OUTLET_ALL_OFF)
-		elif(state == STATE_ON):
-			newWaveForm.extend(OUTLET_ALL_ON)
-	elif(outlet == 1):
-		if(state == STATE_OFF):
-			newWaveForm.extend(OUTLET_1_OFF)
-		elif(state == STATE_ON):
-			newWaveForm.extend(OUTLET_1_ON)
-	elif(outlet == 2):
-		if(state == STATE_OFF):
-			newWaveForm.extend(OUTLET_2_OFF)
-		elif(state == STATE_ON):
-			newWaveForm.extend(OUTLET_2_ON)
-	elif(outlet == 3):
-		if(state == STATE_OFF):
-			newWaveForm.extend(OUTLET_3_OFF)
-		elif(state == STATE_ON):
-			newWaveForm.extend(OUTLET_3_ON)
-	elif(outlet == 4):
-		if(state == STATE_OFF):
-			newWaveForm.extend(OUTLET_4_OFF)
-		elif(state == STATE_ON):
-			newWaveForm.extend(OUTLET_4_ON)
-	elif(outlet == 5):
-		if(state == STATE_OFF):
-			newWaveForm.extend(OUTLET_5_OFF)
-		elif(state == STATE_ON):
-			newWaveForm.extend(OUTLET_5_ON)
-	elif(outlet == 6):
-		if(state == STATE_OFF):
-			newWaveForm.extend(OUTLET_6_OFF)
-		elif(state == STATE_ON):
-			newWaveForm.extend(OUTLET_6_ON)
-
-			
-	return newWaveForm
-
-
-if(len(sys.argv) != 3 or int(sys.argv[2]) > 1):
-	print "Usage: <outlet> <state>"
-	print "For example: python ac_oulet_control.py 1 1"
+if(len(sys.argv) != 4 or int(sys.argv[3]) > 1):
+	print "Usage: <type> <outlet> <state>"
+	print "For example: python ac_oulet_control.py zap 1 1"
 	print "Will turn outlet 1 on."
 else:
 	main(sys.argv)
+
---
+++ python/remotes.py
@@ -0,0 +1,71 @@
+# Python 3.4+
+
+class ByeBye():
+	NAME = "byebye"
+	SIGNAL_SEND_ATTEMPTS = 30	
+	SHORT_DELAY_ON = 330
+	SHORT_DELAY_OFF = 900
+	LONG_DELAY_ON = 920
+	LONG_DELAY_OFF = 305
+	SIGNAL_BEGIN = 2000
+	
+	#SIGNAL CONSTS
+	BASE_WAVE_FORM = [0,1,1,0,1,1,1,0,1,1,0,0,1,1,1,0,1,0,0,1]
+	OUTLET_MAP = {
+		0: [0,0,1,0],
+		1: [1,1,1,0],
+		2: [1,0,1,0],
+		3: [0,1,1,0],
+		4: [1,1,0,0],
+		5: [1,0,0,0],
+		6: [0,0,1,0,0]
+	}
+
+	def createSignal(self, outlet, state):
+		return self.BASE_WAVE_FORM + [state] + self.OUTLET_MAP[outlet]
+
+class Zap:
+	NAME = "zap"
+	
+	SIGNAL_SEND_ATTEMPTS = 30
+	SHORT_DELAY_ON = 147
+	SHORT_DELAY_OFF = 627
+	LONG_DELAY_ON = 535
+	LONG_DELAY_OFF = 247
+	SIGNAL_BEGIN = 6113
+	
+	BASE_WAVE_FORM = [0,0,0,1,0,1,0,1,0,1]
+	BASE_ON_TAIL = [0,0,1,1,0]
+	BASE_OFF_TAIL = [1,1,0,0,0]
+	
+	#[0,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,0,1,1,1,1,0,0,0] # ch 1 off
+	#[0,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,1,0,0,1,1,0,0,0] # ch 2 off
+	#[0,0,0,1,0,1,0,1,0,1,0,1,0,1,1,1,0,0,0,0,1,1,0,0,0] # ch 3 off
+	#[0,0,0,1,0,1,0,1,0,1,0,1,1,1,0,1,0,0,0,0,1,1,0,0,0] # ch 4 off
+	#[0,0,0,1,0,1,0,1,0,1,1,1,0,1,0,1,0,0,0,0,1,1,0,0,0] # ch 5 off
+	
+	#[0,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,0,1,1,0,0,1,1,0] # ch 1 on
+	#[0,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,1,0,0,0,0,1,1,0] # ch 2 on
+	#[0,0,0,1,0,1,0,1,0,1,0,1,0,1,1,1,0,0,0,0,0,0,1,1,0] # ch 3 on
+	#[0,0,0,1,0,1,0,1,0,1,0,1,1,1,0,1,0,0,0,0,0,0,1,1,0] # ch 4 on
+	#[0,0,0,1,0,1,0,1,0,1,0,1,1,1,0,1,0,0,0,0,0,0,1,1,0] # ch 5 on
+
+	CHANNEL_MAP = {
+		1: [0,1,0,1,0,1,0,0,1,1],
+		2: [0,1,0,1,0,1,1,1,0,0],
+		3: [0,1,0,1,1,1,0,0,0,0],
+		4: [0,1,1,1,0,1,0,0,0,0],
+		5: [1,1,0,1,0,1,0,0,0,0]
+	}
+
+	def createSignal(self, outlet, state):
+		if (state == 0):
+			return self.BASE_WAVE_FORM + self.CHANNEL_MAP[outlet] + self.BASE_OFF_TAIL
+		elif (state == 1):
+			return self.BASE_WAVE_FORM + self.CHANNEL_MAP[outlet] + self.BASE_ON_TAIL
+
+
+		print "This shouldn't happen..."
+		return []
+
+
