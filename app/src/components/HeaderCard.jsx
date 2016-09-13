a2a70a28101f5c63d2dec0e788bd98466b584bef
Nick Konecny <nkonecny@etsy.com>
2016-09-12 20:33:55 -0400

    Lux fix

--- app/src/components/HeaderCard.jsx
+++ app/src/components/HeaderCard.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 //https://www.codementor.io/reactjs/tutorial/beginner-guide-setup-reactjs-environment-npm-babel-6-webpack
 export default class HeaderCard extends Component {
   constructor(props) {
-    super(props);  
+    super(props);
     this.state = {
       summary: "Loading",
       hourly_summary: "Loading",
@@ -10,7 +10,7 @@ export default class HeaderCard extends Component {
       home_weather: {
         temperature: "Loading",
         humidity: "Loading",
-        light_level: "Loading",
+        lux: "Loading",
       }
     };
   }
@@ -20,13 +20,13 @@ export default class HeaderCard extends Component {
       url: '/api/v1/weather',
       dataType: 'json',
       cache: false,
-      success: function(data) {        
+      success: function(data) {
         this.setState({
           summary: data.currently.summary,
           hourly_summary: data.hourly.summary,
-          temperature: data.currently.temperature,          
+          temperature: data.currently.temperature,
         });
-        
+
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(status, err.toString());
@@ -41,12 +41,12 @@ export default class HeaderCard extends Component {
       url: 'https://project-3654207232474154346.firebaseio.com/weather.json?limitToLast=1&orderBy="$key"',
       dataType: 'json',
       cache: false,
-      success: function(data) {        
+      success: function(data) {
         this.setState({
-          home_weather: data[Object.keys(data).sort().pop()],        
+          home_weather: data[Object.keys(data).sort().pop()],
         });
 
-        
+
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(status, err.toString());
@@ -55,41 +55,41 @@ export default class HeaderCard extends Component {
 
   }
 
-  componentDidMount = function() {          
-    this.loadWeather();      
-    setInterval(this.loadWeather, 1000*60*10);  
+  componentDidMount = function() {
+    this.loadWeather();
+    setInterval(this.loadWeather, 1000*60*10);
 
     this.loadHomeWeather();
   }
- 
-  render() {  
+
+  render() {
     var imgUrl = "http://cam.sheratontribecaview.com/sheraton-tribeca-new-york-hotel.jpg?" + (new Date()).getTime();
-    var headerImageBackground = {      
+    var headerImageBackground = {
       backgroundImage: 'url(' + imgUrl + ')'
     };
-    
+
     return(
         <div className="card">
             <div className="card-image card-image-height">
               <div className="center-cropped" style={headerImageBackground}>
               </div>
-              
-              <span className="card-title">                
-              </span>              
+
+              <span className="card-title">
+              </span>
             </div>
             <div className="card-content">
               <div className="col s12 m6">
-                <h3>Outside - {this.state.summary}.</h3>                
+                <h3>Outside - {this.state.summary}.</h3>
                 <h5>
                   <div className="row">
                       <div className="col s12">
                         Outside: {this.state.temperature}°. Today, {this.state.hourly_summary}
                       </div>
                   </div>
-                </h5>  
-                  
+                </h5>
+
                 <h3>Inside:</h3>
-                
+
                 <div className="row">
                   <h5>
                     <div className="col s12 m4">
@@ -99,14 +99,14 @@ export default class HeaderCard extends Component {
                       Humidity: {this.state.home_weather.humidity}%
                     </div>
                     <div className="col s12 m4">
-                      Light Level: {this.state.home_weather.light_level} 
+                      Light Level: {this.state.home_weather.lux}
                     </div>
                   </h5>
                 </div>
-                
-                
 
-                
+
+
+
               </div>
             </div>
             <div className="card-action">
