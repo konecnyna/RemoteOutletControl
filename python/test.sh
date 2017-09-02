3694a424df4d3a7455c89234eb278fcd77fbe56f
konecnyna <konecnyna@gmail.com>
2017-09-02 12:27:28 -0400

    Update test.sh

--- python/test.sh
+++ python/test.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 for(( i=1; i<=30; i++ ))
 do
-	STATE=$((i%2))
-	echo $STATE
-	sudo python ac_outlet_control.py zap 2 $STATE;
+        STATE=$((i%2))
+    if [ $STATE == 1  ]
+    then
+        echo "Turning on outlet"
+    else
+        echo "Turning off outlet"
+    fi
+        sudo python ac_outlet_control.py zap 2 $STATE;
 done
