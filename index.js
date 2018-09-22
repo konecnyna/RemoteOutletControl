084d3ebe9da8ce0578413fb7fea2dabecf514e1d
Nick Konecny <nkonecny@etsy.com>
2018-09-22 17:13:37 -0400

    update

--- index.js
+++ index.js
@@ -1,146 +1,147 @@
-var PythonShell = require('python-shell');
-var path = require('path');
-var jsonfile = require('jsonfile');
-var seqqueue = require('seq-queue');
-var fs = require('fs');
-var path = require('path');
-var express = require('express');
-var request = require('request');
+var PythonShell = require("python-shell");
+var path = require("path");
+var jsonfile = require("jsonfile");
+var seqqueue = require("seq-queue");
+var fs = require("fs");
+var path = require("path");
+var express = require("express");
+var request = require("request");
 
-var weather = require('./app/lib/weather.js');
-var jsonHelper = require('./app/lib/jsonHelper.js');
+var weather = require("./app/lib/weather.js");
+var jsonHelper = require("./app/lib/jsonHelper.js");
 var queue = seqqueue.createQueue(1000);
 
 var DEBUG = false;
 
-var localPath = path.join(__dirname, 'python/');
-var pythonFile =  DEBUG ? 'test.py'  : 'ac_outlet_control.py';
+var localPath = path.join(__dirname, "python/");
+var pythonFile = DEBUG ? "test.py" : "ac_outlet_control.py";
 
-
+let thinkEcoApi = false;
 
 var method = RemoteOutletControl.prototype;
 function RemoteOutletControl(app, secrets, route) {
-    if (route) {
-        ROOT_PATH = route;
+  if (thinkEcoApi === false) {
+    thinkEcoApi = new ThinkEcoAPI(
+      secrets.modlet_user,
+      secrets.modlet_pass,
+      console.log
+    );
+  }
+  if (route) {
+    ROOT_PATH = route;
+  } else {
+    ROOT_PATH = "/";
+  }
+  console.log("running on:" + ROOT_PATH);
+
+  if (!secrets) {
+    console.error("No secrets provided to remote outlet control");
+  }
+
+  app.use(ROOT_PATH, express.static(path.join(__dirname, "app/dist")));
+
+  app.get("/api/v1/state", function(req, res) {
+    jsonHelper.getJSON(res, null);
+  });
+
+  app.get("/api/v1/updateJSON", function(req, res) {
+    var outlet = req.query.outlet;
+    var state = req.query.state;
+    if (state === "on" || state === "On") {
+      state = 1;
+    } else if (state === "off" || state === "Off") {
+      state = 0;
     } else {
-        ROOT_PATH = "/";
-    }    
-    console.log("running on:" + ROOT_PATH);
-
-    if (!secrets) {
-        console.error("No secrets provided to remote outlet control");
+      state = parseInt(state);
     }
 
-    app.use(ROOT_PATH, express.static(path.join(__dirname, 'app/dist')));
+    if (outlet === "ac" && req.query.is_auto_ac_request === undefined) {
+      // Auto ac. Anytime ac state changes thats not auto turn off.
+      request(
+        {
+          url: `${secrets.thermostat_update_endpoint}?state=${req.query.state}`
+        },
+        function(error, response, body) {
+          if (error || response.statusCode !== 200) {
+            console.error("Status code: " + response.statusCode, error);
+          }
+        }
+      );
+    }
 
-    app.get("/api/v1/state", function(req, res) {
-        jsonHelper.getJSON(res, null);
-    });
+    if (outlet && (state === 0 || state === 1)) {
+      queue.push(
+        function(task) {
+          runPythonScript(outlet, state, function(data, err) {
+            if (err) {
+              res.status(400);
+              res.json(err);
+              return;
+            }
+            res.send(data);
+            task.done();
+          });
+        },
+        function() {
+          //console.log('task timeout');
+        },
+        5000
+      );
+    } else {
+      console.log("Invalid request");
+      res.status(400);
+      res.json({
+        error: "Invalid request"
+      });
+    }
+  });
 
-    app.get("/api/v1/updateJSON", function(req, res) {
-        var outlet = req.query.outlet;
-        var state = req.query.state;
-        if (state === 'on' || state === 'On') {
-            state = 1;
-        } else if (state === 'off' || state === 'Off') {
-            state = 0;
+  app.post("/api/v1/update_outlets", function(req, res) {
+    try {
+      messageObject = JSON.parse(req.body.outlets);
+      jsonfile.writeFile(jsonHelper.jsonFileName, messageObject, function(err) {
+        if (err) {
+          res.status(400);
+          res.json({
+            error: err
+          });
         } else {
-            state = parseInt(state);
+          res.status(200);
+          res.json("Saved");
+        }
+      });
+    } catch (e) {
+      console.log("hi", e);
+      res.status(400);
+      res.json({
+        error: "Invalid request"
+      });
+    }
+  });
+
+  function runPythonScript(outlet, state, callback) {
+    jsonHelper.getJSON(null, function(jsonArray) {
+      var PythonShell = require("python-shell");
+      var options = {
+        scriptPath: localPath,
+        mode: "text",
+        args: [jsonArray[outlet].type, jsonArray[outlet].outlet_number, state]
+      };
+
+      var pyshell = new PythonShell(pythonFile, options);
+      pyshell.end(function(err) {
+        if (err) {
+          callback(null, err);
+          return;
         }
 
-        if (outlet === "ac" && req.query.is_auto_ac_request === undefined) {
-            // Auto ac. Anytime ac state changes thats not auto turn off.
-            request({url: this.secrets.thermostat_update_endpoint}, function (error, response, body) {
-                if (error || response.statusCode !== 200) {
-                    console.error("Status code: " + response.statusCode, error);
-                }
-            });   
-        }
-
-        if (outlet && (state === 0 || state === 1)) {
-            queue.push(
-                function(task) {
-                    runPythonScript(outlet, state, function(data, err) {
-                        if (err) {
-                            res.status(400);
-                            res.json(err);
-                            return;
-                        }
-                        res.send(data);
-                        task.done();
-                    });
-                },
-                function() {
-                    //console.log('task timeout');
-                }, 5000);
-
-
-        } else {
-            console.log("Invalid request");
-            res.status(400);
-            res.json({
-                error: "Invalid request"
-            });
-        }
-    });
-
-
-    app.post('/api/v1/update_outlets', function(req, res) {
-        try {
-            messageObject = JSON.parse(req.body.outlets);
-            jsonfile.writeFile(jsonHelper.jsonFileName, messageObject, function(err) {
-                if (err) {
-                    res.status(400);
-                    res.json({
-                        error: err
-                    });
-                } else {
-                    res.status(200);
-                    res.json("Saved");
-                }
-            });
-
-        } catch (e) {
-            console.log("hi",e);
-            res.status(400);
-            res.json({
-                error: "Invalid request"
-            });
-        }
-
-    });
-
-
-    function runPythonScript(outlet, state, callback) {
-        jsonHelper.getJSON(null, function(jsonArray) {
-            var PythonShell = require('python-shell');
-            var options = {
-                scriptPath: localPath,
-                mode: 'text',
-                args: [
-                    jsonArray[outlet].type, 
-                    jsonArray[outlet].outlet_number, 
-                    state
-                ]
-            };
-
-            var pyshell = new PythonShell(pythonFile, options);
-            pyshell.end(function(err) {
-                if (err) {
-                    callback(null, err);
-                    return;
-                }
-
-                jsonHelper.updateJSONStates(outlet, state, jsonArray, function(json_data) {
-                    callback(json_data);
-                });
-
-            });
-
-
+        jsonHelper.updateJSONStates(outlet, state, jsonArray, function(
+          json_data
+        ) {
+          callback(json_data);
         });
-
-    }
-
+      });
+    });
+  }
 }
 module.exports = RemoteOutletControl;
--- server.js
+++ server.js
@@ -1,45 +1,46 @@
 // REACT
-const webpack = require('webpack');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('./app/webpack.config.js');
-var OutletControl = require('./index.js');
+const webpack = require("webpack");
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpackHotMiddleware = require("webpack-hot-middleware");
+const config = require("./app/webpack.config.js");
+var OutletControl = require("./index.js");
 
-const express = require('express');
+const express = require("express");
 var app = express();
 
-var path = require('path');
+var path = require("path");
 const isDeveloping = false;
 const port = isDeveloping ? 3000 : 3000;
 
-
 if (isDeveloping) {
-    const compiler = webpack(config);
-    const middleware = webpackMiddleware(compiler, {
-        publicPath: config.output.publicPath,
-        contentBase: 'src',
-        stats: {
-            colors: true,
-            hash: false,
-            timings: true,
-            chunks: false,
-            chunkModules: false,
-            modules: false
-        }
-    });
+  const compiler = webpack(config);
+  const middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: "src",
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  });
 
-    app.use(middleware);
-    app.use(webpackHotMiddleware(compiler));
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
 } else {
-    app.use(express.static(path.join(__dirname, 'dist')));
+  app.use(express.static(path.join(__dirname, "dist")));
 }
 
-
-app.listen(port, '0.0.0.0', function onStart(err) {
-    if (err) {
-        console.log(err);
-    }
-    new OutletControl(app, {}, false);
-    console.info('==> 🌎 Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
+app.listen(port, "0.0.0.0", function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  new OutletControl(app, {}, false);
+  console.info(
+    "==> 🌎 Listening on port %s. Open up http://localhost:%s/ in your browser.",
+    port,
+    port
+  );
 });
-
